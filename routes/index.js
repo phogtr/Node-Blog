68a2429f6cf32b4ede1b1947bd7c428dc5a2da5a
phogtr <phogbtr@gmail.com>
2020-07-06 04:02:06 -0700

    implemented advance search

--- routes/index.js
+++ routes/index.js
@@ -4,19 +4,50 @@ const router = express.Router();
 const User = require("../models/User.js");
 const Post = require("../models/Post.js");
 
+// home page
 router.get("/", (req, res) => {
-  let search = {};
-  if (req.query.title != null && req.query.title !== "") {
-    search.title = new RegExp(req.query.title, "i");
+  let searchTitle = {};
+  let searchAuthor = {};
+
+  if (req.query.search != null && req.query.search !== "") {
+    if (req.query.selectSearch == "title") {
+      searchTitle.title = new RegExp(req.query.search, "i");
+    } else {
+      searchAuthor.name = new RegExp(req.query.search, "i");
+    }
   }
-  Post.find(search, (err, posts) => {
+  Post.find(searchTitle, (err, posts) => {
     if (err) throw err;
-    User.find({}, (err, users) => {
+    User.find(searchAuthor, (err, users) => {
       if (err) throw err;
       res.render("index", {
         posts: posts,
         users: users,
-        search: req.query,
+      });
+    });
+  });
+});
+
+// advance search
+router.get("/advanceSearch", (req, res) => {
+  let searchTitle = {};
+  let searchAuthor = {};
+
+  if (req.query.title != null && req.query.title !== "") {
+    searchTitle.title = new RegExp(req.query.title, "i");
+  }
+
+  if (req.query.author != null && req.query.author !== "") {
+    searchAuthor.name = new RegExp(req.query.author, "i");
+  }
+
+  Post.find(searchTitle, (err, posts) => {
+    if (err) throw err;
+    User.find(searchAuthor, (err, users) => {
+      if (err) throw err;
+      res.render("index", {
+        posts: posts,
+        users: users,
       });
     });
   });
